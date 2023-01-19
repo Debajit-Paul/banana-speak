0e92645255f9081b95ec7f84de9a85bde8437345
Debajit Paul <debajitfan.of@gmail.com>
2023-01-19 17:56:56 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+var translate_btn=document.querySelector("#btn-translate");
+var InputText=document.querySelector("#txtInput");
+var outputText=document.querySelector("#output");
+
+
+
+translate_btn.addEventListener("click", clickEventHandler())
